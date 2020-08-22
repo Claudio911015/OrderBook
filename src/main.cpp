ed5a944e73f17c05a618b2a154cf02b7241a892a
Claudio <Hola3241#>
2020-08-21 20:54:46 -0500

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <OrderBook.h>
+#include <order.h>
+
+#include <memory>
 
 int main() {
 
-    //OrderBook b(5);
+    std::shared_ptr<order> nn(new order(5));
+
+    order o(5);
     OrderBook bx(3);
     std::cout<<"Hola \n";
     return 0;
